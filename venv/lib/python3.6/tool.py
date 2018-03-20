ede8d4a58aac8b711428972802319a700bb713d7
jkinville <jkinville@newforming.com>
2018-03-20 16:09:01 -0400

    Initial commit

--- venv/lib/python3.6/tool.py
+++ venv/lib/python3.6/tool.py
@@ -2,12 +2,12 @@ from jenkinsapi.jenkins import Jenkins
 
 def get_server_instance():
     jenkins_url = 'https://prod-jenkins.newforma.io/'
-    server = Jenkins(jenkins_url, username = 'jkinville', password = '8c3423388555cb0e1b53357a7db585d57302d37e')
+    server = Jenkins(jenkins_url, username = 'USERNAME_HERE', password = 'PASSWORD_HERE')
     jobs = server.get_jobs_info()
     for job in jobs:
-        job_instance = server.get_job('Newforma/cloud-action-items-api/acceptance-tests')
+        job_instance = server.get_job(job[1])
         latestBuild = job_instance.get_last_build()
-        print(latestBuild.get_status())
+        print(job[0] + ' ' + latestBuild.get_status())
     return server
 
 if __name__ == '__main__':
